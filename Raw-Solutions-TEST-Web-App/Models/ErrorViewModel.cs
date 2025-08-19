5c5c0c8abef138e20155d874f4f77504469d9e03
alexk <alexk@ALEX-DESKTOP>
2025-08-19 16:02:47 +1200

    Add project files.

---
+++ Raw-Solutions-TEST-Web-App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Raw_Solutions_TEST_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
